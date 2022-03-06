b813ff55e2140f27564ba1a7af9a9329746d82e4
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-03-06 13:19:31 +0100

    Subida de ejercicio 1.2 y 1.3

--- Aitor/Ejercicio1_actividad2.m
+++ Aitor/Ejercicio1_actividad2.m
@@ -1,1 +1,61 @@
 clear;close all;clc;
+
+f      = 5800e6;
+c      =3e8;
+lambda = c/f;
+
+% Alturas, distancia y radio en metros
+
+d  = 20.09e3; %en Km
+R0 = 6370e3;
+
+e  = [786 800 803 799 735 760 788 795];
+a  = [10 0 0 0 0 0 0  10];
+d1 = [0 0.806e3 1.910e3 3.721e3 7.831e3 10.955e3 14.965e3 d];
+d2 = d - d1;
+
+% -------------------------------------------------------------------------
+k    = 4/3;
+Re   = R0*k;
+dmax = sqrt(2*Re)*(sqrt(e(1)+a(1))+sqrt(e(end)+a(end))); 
+
+if(d<0.1*dmax) 
+    %Código ejecutado si tierra plana
+    "Tierra plana"
+else
+    %Código ejecutado si tierra curva
+    "Tierra curva"
+end
+% -------------------------------------------------------------------------
+
+K           = [0.5,2/3,1,4/3];
+Re          = R0*K;
+altura_rayo = ((e(end)+a(end)-e(1)-a(1))/d)*d1 + e(1)+a(1);
+R1          = sqrt(lambda*d1.*d2/d); 
+
+%Repito el código de Diego pero "K" es un vector, y por tanto "Re" tambien.
+numero_iteraciones    = size(Re);
+columnas              = size(d1);
+flecha_iterada        = zeros(numero_iteraciones(2),columnas(2));
+despejamiento_iterado = zeros(numero_iteraciones(2),columnas(2));
+uve_iterado           = zeros(numero_iteraciones(2),columnas(2));
+Ldif_iterado          = zeros(numero_iteraciones(2),columnas(2));
+
+for iteracion=1:numero_iteraciones(2)
+    
+    flecha_iterada(iteracion,:)        = (d1.*d2)/(2*Re(iteracion));
+    despejamiento_iterado(iteracion,:) = e + flecha_iterada(iteracion,:) - altura_rayo;
+    uve_iterado(iteracion,:)           = sqrt(2)*despejamiento_iterado(iteracion,:)./R1;
+    Ldif_iterado(iteracion,:)          = 6.9 + 20*log10(sqrt((uve_iterado(iteracion,:)-0.1).^2 +1) + uve_iterado(iteracion,:)-0.1);
+    
+    hold on
+    plot(Ldif_iterado(iteracion,:))
+    xticks([2:7])
+    hold off
+end
+
+ylabel("Pérdidas por difracción dB ");
+xlabel("Obstáculo")
+legend("Difracción K = 1/2","Difracción K = 2/3","Difracción K = 1","Difracción K = 4/3")
+
+
