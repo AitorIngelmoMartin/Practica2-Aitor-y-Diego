41284856f6c17e706be254bfb6204960ae932e6b
diegodelreal <62442321+diegodelreal@users.noreply.github.com>
2022-03-28 19:17:08 +0200

    actualizados

--- Diego/Practica 2/Ejercicio1.m
+++ Diego/Practica 2/Ejercicio1.m
@@ -21,7 +21,7 @@
 
 clear;clc;
 
-f = 5800e6;
+f = 2300e6;
 c=3e8;
 lambda= c/f;
 
@@ -31,24 +31,14 @@ lambda= c/f;
 d = 20.09e3; %en Km
 R0 =6370e3;
 
-e = [786 800 803 799 735 760 788 795];
-a = [10 0 0 0 0 0 0  10];
+e = [796 800 803 799 735 760 788 805];
+a = [17 0 0 0 0 0 0 12];
 d1 = [0 0.806e3 1.910e3 3.721e3 7.831e3 10.955e3 14.965e3 d];
 d2 = d - d1;
 
 % -------------------------------------------------------------------------
-k = 4/3;
+k = 2/3;
 Re = R0*k;
-dmax = sqrt(2*Re)*(sqrt(e(1)+a(1))+sqrt(e(end)+a(end))); 
-
-if(d<0.1*dmax) 
-    %Código ejecutado si tierra plana
-    "Tierra plana"
-else
-    %Código ejecutado si tierra curva
-    "Tierra curva"
-end
-% -------------------------------------------------------------------------
 
 %Como hay obstáculos, solo existen pérdidas por difracción
     "Hay pérdidas por difracción"
@@ -63,6 +53,4 @@ end
     R1 = sqrt(lambda*d1.*d2/d); %Altura del primer rayo de Fresnel
     uve = sqrt(2)*despejamiento./R1;
 
-    %Porcentaje despejamiento
-    FZ1 = despejamiento./R1;
-    FZ1min = FZ1(3);
+    porcentaje = (despejamiento./R1)*100;
