0036ee52c75ea69484d82bb24874366a0cd3aa6b
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-03-30 22:38:04 +0200

    Arreglo de UVE subvanos

--- Aitor/entregable_2_1.m
+++ Aitor/entregable_2_1.m
@@ -62,3 +62,60 @@ for iteracion=1:numero_iteraciones(2)
 end
 ylabel("Valor coeficiente");xlabel("Obstáculo");
 legend("Difracción K = 1/2","Difracción K = 2/3","Difracción K = 1","Difracción K = 4/3");
+
+% ********
+
+%SUBVANO IZQ---------
+
+Distancia_IZQ =1910;
+D1_IZQ = 806;
+D2_IZQ = Distancia_IZQ - D1_IZQ;
+e_IZQ            = 800;
+
+h2_IZQ = 803;
+h1_IZQ = 796+10;
+
+AlturaRayo_IZQ   = ((h2_IZQ-h1_IZQ)/Distancia_IZQ)*D1_IZQ + h1_IZQ;
+
+for(iteracion=1:4)
+    Flecha_IZQ(iteracion)  = (D1_IZQ*D2_IZQ)/(2*K(iteracion)*R0);
+    Despejamiento_IZQ(iteracion)      = Flecha_IZQ(iteracion) + e_IZQ-AlturaRayo_IZQ;
+end
+
+Rfresnell_IZQ     = sqrt((lambda*D1_IZQ*D2_IZQ)/(D1_IZQ+D2_IZQ));
+
+Difracc_IZQ       = sqrt(2)*(Despejamiento_IZQ/Rfresnell_IZQ)
+
+% for(iteracion=1:4)
+%  Ldif_iterado(iteracion)         =  6.9 + 20*log10(sqrt((Difracc_O1(iteracion)-0.1).^2 +1) + Difracc_O1(iteracion)-0.1);
+% end
+
+% ********
+
+
+%SUBVANO DRCH---------
+
+Distancia_DRCH =18180;
+D1_DRCH = 1811;
+D2_DRCH= Distancia_DRCH - D1_DRCH;
+e_DRCH            = 799;
+
+h2_DRCH = 805+8;
+h1_DRCH = 803;
+
+AlturaRayo_DRCH   = ((h2_DRCH-h1_DRCH)/Distancia_DRCH)*D1_DRCH + h1_DRCH;
+
+for(iteracion=1:4)
+    Flecha_DRCH(iteracion)  = (D1_DRCH*D2_DRCH)/(2*K(iteracion)*R0);
+    Despejamiento_DRCH(iteracion)      = Flecha_DRCH(iteracion) + e_DRCH-AlturaRayo_DRCH;
+end
+
+Rfresnell_DRCH     = sqrt((lambda*D1_DRCH*D2_DRCH)/(D1_DRCH+D2_DRCH));
+
+Difracc_DRCH       = sqrt(2)*(Despejamiento_DRCH/Rfresnell_DRCH)
+
+% for(iteracion=1:4)
+%  Ldif_iterado(iteracion)         =  6.9 + 20*log10(sqrt((Difracc_O1(iteracion)-0.1).^2 +1) + Difracc_O1(iteracion)-0.1);
+% end
+
+% ********
