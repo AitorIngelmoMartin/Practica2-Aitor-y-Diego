524a397be33653cfb16c6d12e0080ceca74e9708
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-05-01 18:41:21 +0200

    Subida script calculo iterativo

--- Aitor/Caso práctico/Prueba_3_vanos.m
+++ Aitor/Caso práctico/Prueba_3_vanos.m
@@ -6,10 +6,9 @@ MTTR = 24;
 
 f         = 23e9;
 lambda    = 3e8/(f);
-Distancia = 18.46e3;
+Distancia = (55.1e3)/3;
 
-R_punto_medio_inferior = 40.754813773852334;
-R_punto_medio_superior =39.198960977634940;
+R_punto_medio =40.141518816862316;
 
 Ptx_dBm = 15.5;
 G_dB = 43.6;
@@ -27,7 +26,7 @@ f        = f/(1e9);Distancia = Distancia/1000;
 K_lluvia = 0.1286;
 Alpha    = 1.0214;     
  
-R_001_total = [R_punto_medio_inferior R_punto_medio_superior]
+R_001_total = R_punto_medio;
 Gamma_r     = K_lluvia*(R_001_total.^(Alpha)) %dB/Km
 Deff        = (Distancia)./(0.477*(Distancia^0.633)*(R_001_total.^(0.073*Alpha))*(f^(0.123))-10.579*(1-exp(-0.024*Distancia))) %Km
 
@@ -47,9 +46,9 @@ MD_dB = Margen_dBm
 logaritmo = log10(MD_dB./(F_001*C1));
 
 soluciones_x =  [( -C2 + sqrt( C2*C2 -4*logaritmo*C3 ) )/(2*C3),( -C2 - sqrt( C2*C2 -4*logaritmo*C3 ) )/(2*C3)];
-x1 =max(soluciones_x(1,1),soluciones_x(1,3));
-x2 =max(soluciones_x(1,2),soluciones_x(1,4));
-x = [x1 x2];
+x =max(soluciones_x);
+
+
 q_calculado = 10.^x
  
  
