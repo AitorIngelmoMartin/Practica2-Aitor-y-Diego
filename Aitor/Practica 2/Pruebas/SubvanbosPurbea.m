dc1439c1553e9f54e142aaece0e78b06cd558a7b
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-05-03 18:15:24 +0200

    Practica final

--- Aitor/Practica 2/Pruebas/SubvanbosPurbea.m
+++ Aitor/Practica 2/Pruebas/SubvanbosPurbea.m
@@ -8,7 +8,7 @@ R0     = 6370e3;
 Re     = R0*K;
 h1     = 807;
 
-e  = [796 800 803 799 735 760 788 805];
+e  = [796 800 811 799 735 760 788 805];
 a  = [10 0 0 0 0 0 0 8];
 d1 = [0 0.806e3 1.910e3 3.721e3 7.831e3 10.955e3 14.965e3 d];
 d2 = d - d1;
@@ -48,7 +48,7 @@ D1_IZQ = 806;
 D2_IZQ = Distancia_IZQ - D1_IZQ;
 e_IZQ            = 800;
 
-h2_IZQ = 803;
+h2_IZQ = e(2);
 h1_IZQ = 796+10;
 
 AlturaRayo_IZQ   = ((h2_IZQ-h1_IZQ)/Distancia_IZQ)*D1_IZQ + h1_IZQ;
@@ -70,7 +70,7 @@ D2_DRCH= Distancia_DRCH - D1_DRCH;
 e_DRCH            = 799;
 
 h2_DRCH = 805+8;
-h1_DRCH = 803;
+h1_DRCH = e(2);
 
 AlturaRayo_DRCH   = ((h2_DRCH-h1_DRCH)/Distancia_DRCH)*D1_DRCH + h1_DRCH;
 
