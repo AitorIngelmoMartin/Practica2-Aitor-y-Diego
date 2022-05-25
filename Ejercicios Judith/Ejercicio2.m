b65fde2cf42b870dee4d0b98850e063f3fd0cf63
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-05-25 16:21:23 +0200

    Subida ejercicios satelites

--- Ejercicios Judith/Ejercicio2.m
+++ Ejercicios Judith/Ejercicio2.m
@@ -4,21 +4,18 @@ Boltzmann       = 1.38e-23;
 Distancia       = 38200e3;
 Rb              = 34e6;
 BW_total        = 36e6;
-
+BW_guarda       = 0.15e6;
 M               = 16;
 % LLuvia estación A
 F_001_A = 9;
 %LLuvia estación B
 F_001_B = 16;
 
-Eb_No_total_min_dB = 12;
-Eb_No_total_min=10^(Eb_No_total_min_dB/10);
+Eb_No_total_dB = 12;
  
 %Si el sistema está por debajo de este Eb/No
 f_ascendente   = 14e9;
-lambda_ascendente = 3e8/f_ascendente;
 f_descendente  = 11e9;
-lambda_descendente = 3e8/f_descendente;
 
 Lad_up_dB      = 0.2;
 Lad_down_dB    = 0.3;
@@ -33,11 +30,11 @@ PIRE_satelite_dBw = 48;
 % 1)Si la modulación utilizada es 16-PSK. ¿Cuál será el valor del factor de roll-off del
 % filtro de coseno alzado utilizado en la transmisión si se utiliza una codificación 
 % contra errores FEC 1/3? Existe una banda de guarda en los extremos de la banda de 0,15 MHz.
-FEC = (1/3);
-BW_guarda = 0.15*1e6;
-BW_portadora = BW_total-2*BW_guarda;
 
-Roll_off     = (BW_portadora-1)/((Rb*(1/FEC))/(log2(M)))-1;
+FEC =1/3;
+
+BW_portadora = BW_total-2*BW_guarda;
+Roll_off     = (BW_portadora)/((Rb*(1/FEC))/(log2(M)))- 1;
 
 % 2)Obtenga el  parámetro de transmisión de las estaciones terrenas de ambas zonas 
 % climáticas para que el enlace funcione con la calidad requerida durante
@@ -45,17 +42,13 @@ Roll_off     = (BW_portadora-1)/((Rb*(1/FEC))/(log2(M)))-1;
 % dispositivos UPC y CAG adecuados a cada zona climática.
 
 %       Parámetros de transmisiñon son PIREet y G/Tet, que dan la C/N0total_minima
+Eb_No_total   = 10^(Eb_No_total_dB/10);
+C_N0_total    = Eb_No_total*Rb;
+C_N0_total_dB = 10*log10(C_N0_total);
 
+Lbf_down_dB  = 20*log10((4*pi*Distancia)/(3e8/f_descendente));
+C_N0_down_dB = PIRE_satelite_dBw - Lbf_down_dB - Lad_down_dB - Margen_down_dB + G_T_estacion - 10*log10(Boltzmann);
 
-Lbf_ascendente_dB  = 20*log10((4*pi*Distancia)/lambda_ascendente);
-Lbf_descendente_dB = 20*log10((4*pi*Distancia)/lambda_descendente);
-CNO_total    = Eb_No_total_min*Rb
-CNO_total_dB = 10*log10(CNO_total);
-
-CN0_descendente = PIRE_satelite_dBw-Lbf_descendente_dB-Lad_up_dB-Margen_up_dB+G_T_estacion-10*log10(Boltzmann);
-
-CN0_ascendente    = 1/((10^(-CNO_total_dB/10))-(10^(-CN0_descendente/10)));
-CN0_ascendente_dB = 10*log10(CN0_ascendente);
-
-PIRE_terrestre = CN0_ascendente_dB+Lbf_ascendente_dB+Lad_up_dB+Margen_up_dB-G_T_satelite+10*log10(Boltzmann)
+C_N0_up    = 1/((10^(-C_N0_total_dB/10))-(10^(-C_N0_down_dB/10)));
+C_N0_up_dB = 10*log10(C_N0_up)
 
--- Ejercicios Judith/Ejercicio5.m
+++ Ejercicios Judith/Ejercicio5.m
@@ -3,6 +3,8 @@ clc;clear;close all;
 % Se desea planificar un sistema FDMA de comunicaciones por satélite en banda Ku (14GHz/12 GHz)
 f_up   = 14e9;
 f_down = 12e9;
+lambda_up = 3e8/f_up;
+lambda_down = 3e8/f_down;
 %8 estaciones terrenas, con 4 que transmiten a 3Mbps, 3 a 5Mbps y 1 a 10Mbps. 
 Rb = [3 3 3 3 5 5 5 10]* 1e6;
 
@@ -22,34 +24,32 @@ Distancia = 39000e3;
 % Los parámetros del satélite son:
     BW_transpondedor     = 36e6;
     BW_guarda            = 1.5e6;
-    SFD_dBW              = -98;
-    G_T_sat_dB               = 10;
+    Flujo_satelite_dBw   = -98;
+    G_T_sat_dB           = 10;
     PIRE_saturacion_dBw  = 52; 
 % Los parámetros de las estaciones terrenas son:
-    G_antena_dB = 40;
-    Tantena     = 22;
-    Lt_dB       = 0.3;
-    G_LNA_dB    = 40;
-    FLNA_dB     = 0.5;
+    G_terrena_dB = 40;
+    Tantena      = 22;
+    Lt_dB        = 0.3;
+    G_LNA_dB     = 40;
+    FLNA_dB      = 0.5;
     
 T0 = 290;
 Ttotal_receptor = Tantena +T0*(10^(Lt_dB/10)-1) + T0*(10^(FLNA_dB/10)-1)*10^(Lt_dB/10)
     
 % 1)Calcule la PIRE en cielo claro para cada estación terrena para el punto de saturación.
-Eb_No = 11.7; % mirado en tabla
-C_N_total = Eb_No + 10*log10(Rb)
+Eb_No        = 11.7; % mirado en tabla
+C_N_total    = Eb_No + 10*log10(Rb);
+BW_portadora = (1+roll_off)*(Rb*(1/FEC))/log2(M);
+Lbf_up_dB    = 20*log10((4*pi*Distancia)/lambda_up);
+Lbf_down_dB  = 20*log10((4*pi*Distancia)/lambda_down);
 
-Lbf_up_dB   = 20*log10((4*pi*Distancia)/f_up)
-Lbf_down_dB = 20*log10((4*pi*Distancia)/f_down)
 
-PIRE_cielo_claro = C_N_total + Lbf_up_dB + Lgas_up_dB + Margen_up_dB - G_T_sat_dB + 10*log10(Boltzmann)
+% + 0 + 10*log10(Rb/sum(Rb)) es el IBO portadora
 
 
 % 2) Calcular la G/T de las estaciones terrenas.
-
-G_T_terrena_dB  = C_N_total + Lbf_up_dB + Lgas_up_dB + Margen_up_dB - PIRE_saturacion_dBw  + 10*log10(Boltzmann)
-
-
+G_T_terrena_dB  = G_terrena_dB - 10*log10(Ttotal_receptor)
 
 % 3)Si el punto de trabajo óptimo es el definido por un IBO=-4 dB y 
 % un OBO=-1,3 dB de salida, y la C/I0 para ese punto de trabajo es para todas
@@ -58,24 +58,24 @@ G_T_terrena_dB  = C_N_total + Lbf_up_dB + Lgas_up_dB + Margen_up_dB - PIRE_satur
 % de las estaciones, y en caso de que para alguna de ellas no se 
 % cumpla, proponga posibles soluciones para esa estación concreta
 
-
 IBO_tpdr  = -4;
 OBO_tpdr  = -1.3;
 C_I0 = 83;
 
-BW_portadora        = (1 + roll_off)*(Rb*FEC/log2(M));
-BW_total_portadoras = BW_portadora
-IBO_ptdr = IBO_tpdr + 10*log10(BW_portadora/BW_total_portadoras);
-OBO_ptdr = OBO_tpdr + 10*log10(BW_portadora/BW_total_portadoras);
+Eb_N0 = 11.5;
+C_N0_objetivo = Eb_N0 + 10*log10(Rb);
+
+PIRE_total = Flujo_satelite_dBw+10*log10(4*pi*(Distancia^2))+Lgas_up_dB+Margen_up_dB;
+PIRE_up    = PIRE_total+10*log10(BW_portadora/sum(BW_portadora));
+PIRE_down  = PIRE_saturacion_dBw +10*log10(BW_portadora/sum(BW_portadora));
 
 
-Lad_down_dB  = Lgas_down_dB + Margen_down_dB;
-PIRE_sat_dBW = SFD_dBW + 10*log10(4*pi*Distancia^2) + Lad_down_dB;
+C_N0_up   = PIRE_up   + IBO_tpdr - Lbf_up_dB - Lgas_up_dB   - Margen_up_dB   + G_T_sat_dB     - 10*log10(Boltzmann);
+C_N0_down = PIRE_down + OBO_tpdr - Lbf_up_dB - Lgas_down_dB - Margen_down_dB + G_T_terrena_dB - 10*log10(Boltzmann);
 
-Lad_up_dB   = Lgas_up_dB + Margen_up_dB;
-PIRE_et_dBW   = SFD_dBW - 10*log10(4*pi*Distancia^2) - Lad_up_dB ;
+BW_guarda = 2*(1.5e6);
+BW_total_portadoras = 36e6 - BW_guarda;
 
-C_N0_up_dB   = PIRE_et_dBW  + IBO_ptdr - Lbf_up_dB   - Lad_up_dB   - Margen_up_dB + G_T_sat_dB     - 10*log10(Boltzmann);
-C_N0_down_dB = PIRE_sat_dBW + OBO_ptdr - Lbf_down_dB - Lad_down_dB - Margen_down_dB  + G_T_terrena_dB - 10*log10(Boltzmann);
 
-c_n0_total  = 1./(1./(10.^(C_N0_up_dB./10))+ 1./(10.^(C_N0_down_dB./10)) + 1./(10.^(C_I0./10)));
+c_n0_total  = 1./(1./(10.^(C_N0_up./10))+ 1./(10.^(C_N0_down./10)) + 1./(10.^(C_I0./10)));
+C_N0_total  = 10*log10(c_n0_total);
